7b0e911e88932cb1aa51c2205c1acfe7656356bf
saruulgerel <saruulgerel.o325@gmail.com>
2025-01-05 01:38:15 +0800

    .

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import compression from 'compression';
+import compression from "compression";
 import bodyParser from "body-parser";
 import cors from "cors";
 import path from "path";
