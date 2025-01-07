2ae5f82303bf9dc49757bff26061e89f73af22a3
saruulgerel <saruulgerel.o325@gmail.com>
2025-01-07 11:31:25 +0800

    PWA

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,48 @@
-const CACHE_NAME = "offline-cache-v2";
-const OFFLINE_URL = "./offline.html";
+// This is the "Offline page" service worker
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.add(OFFLINE_URL);
-    })
-  );
-  self.skipWaiting();
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
+
+const CACHE = "pwabuilder-page";
+
+const offlineFallbackPage = "./offline.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
+
+self.addEventListener("install", async (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(OFFLINE_URL);
-    })
-  );
-});
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResp = await event.preloadResponse;
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
+          if (preloadResp) {
+            return preloadResp;
           }
-        })
-      );
-    })
-  );
+
+          const networkResp = await fetch(event.request);
+          return networkResp;
+        } catch (error) {
+          const cache = await caches.open(CACHE);
+          const cachedResp = await cache.match(offlineFallbackPage);
+          return cachedResp;
+        }
+      })()
+    );
+  }
 });
