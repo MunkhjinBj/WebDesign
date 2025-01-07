c7d0cbe4a0d53c4b779a8fda60ca1d2f8a6ff9ae
saruulgerel <saruulgerel.o325@gmail.com>
2025-01-07 11:05:35 +0800

    .

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = 'offline-cache-v1';
-const OFFLINE_URL = './offline.html';
+const CACHE_NAME = "offline-cache-v2";
+const OFFLINE_URL = "./offline.html";
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.add(OFFLINE_URL);
     })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request).catch(() => {
       return caches.match(OFFLINE_URL);
@@ -18,11 +18,11 @@ self.addEventListener('fetch', event => {
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cache => {
+        cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
             return caches.delete(cache);
           }
@@ -30,5 +30,4 @@ self.addEventListener('activate', event => {
       );
     })
   );
-  self.clients.claim();
 });
